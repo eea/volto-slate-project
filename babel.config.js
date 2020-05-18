fcf9ef35e02d80ec2a720707cb57bb5be02fd607
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-05-18 18:29:24 +0300

    Make tests work

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,46 @@
-module.exports = require('@plone/volto/babel');
+// module.exports = require('@plone/volto/babel');
+module.exports = function (api) {
+  api.cache(true);
+  const presets = ['razzle/babel'];
+  const plugins = [
+    'lodash',
+    '@babel/plugin-proposal-function-bind',
+    '@babel/plugin-proposal-do-expressions',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-function-sent',
+    '@babel/plugin-proposal-json-strings',
+    '@babel/plugin-proposal-logical-assignment-operators',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-optional-chaining',
+    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+    '@babel/plugin-proposal-throw-expressions',
+    '@babel/plugin-syntax-import-meta',
+    '@babel/plugin-syntax-dynamic-import',
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathSuffix: 'src',
+      },
+    ],
+    [
+      'react-intl',
+      {
+        messagesDir: './build/messages/',
+      },
+    ],
+    '@loadable/babel-plugin',
+  ];
+
+  return {
+    plugins,
+    presets,
+  };
+};
