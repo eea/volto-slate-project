0ef80607824052bd1707a8fef7a93c1b9ad4ed87
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-05-21 18:07:16 +0300

    Update get_correct_pwd.sh

--- get_correct_pwd.sh
+++ get_correct_pwd.sh
@@ -3,7 +3,7 @@
 
 docker_id=$(cat /proc/self/cgroup | grep :memory: | sed  's#.*/\([0-9a-fA-F]*\)$#\1#' )
 
-if [ -e $docker_id ] && [ $(echo $docker_id | grep -c memory ) -gt 0 ]; then
+if [ -e $docker_id ] && [ $(echo $docker_id | grep -c memory ) -eq 0 ]; then
 	REPORTDIR=$(docker inspect $docker_id | grep :/var/jenkins_home/worker | awk -F'["|:]' '{print $2}')
         pwd | sed -e "s#/var/jenkins_home/worker#$REPORTDIR#"
 else	
