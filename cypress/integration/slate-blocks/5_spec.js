2895b622f6f2484287f21fce979df70dd5aa84d4
Silviu Bogan <silviubogan@gmail.com>
2020-06-15 17:50:30 +0300

    Added another passing test and an unimplemented test

--- cypress/integration/slate-blocks/5_spec.js
+++ cypress/integration/slate-blocks/5_spec.js
@@ -7,9 +7,61 @@ import {
 } from '../../support';
 
 if (Cypress.env('API') !== 'guillotina') {
-  describe('Not implemented tests', () => {
+  describe('Slate.js Volto blocks', () => {
     beforeEach(slateBeforeEach);
 
-    it('should create a block with a numbered list with a single item, move the cursor to the middle of the item, insert line break, have two items, the first one with the left part of the first item, and the second one with the right part of the initial first item, press again line break, have an empty second item ith cursor at the beginning of the third, press line break again and have 2 blocks with the first item one, and the rest in the second block', () => {});
+    it('should create a block with a numbered list with a single item, move the cursor to the end of the item, insert line break, have two items, the cursor on the last one, which is empty, and then another line break should create a new block which is of type `paragraph`', () => {
+      let s1 = createSlateBlock();
+
+      s1.typeInSlate('hello, world');
+
+      // select all contents of slate block
+      // - this opens hovering toolbar
+      cy.contains('hello, world').then((el) => {
+        selectSlateNodeOfWord(el);
+      });
+
+      // this is the numbered list option in the hovering toolbar
+      cy.get('.slate-inline-toolbar > :nth-child(8)').click();
+
+      // move the text cursor
+      getSelectedSlateEditor().type('{rightarrow}');
+
+      // simulate pressing Enter
+      getSelectedSlateEditor().lineBreakInSlate();
+
+      // simulate pressing Enter again
+      getSelectedSlateEditor().lineBreakInSlate();
+
+      // there should be 2 slate blocks on the page
+      cy.get('.block-editor-slate').should('have.length', 2);
+
+      getSlateBlockValue(cy.get('.slate-editor').first()).should('deep.eq', [
+        {
+          type: 'numbered-list',
+          children: [
+            {
+              type: 'list-item',
+              children: [
+                {
+                  text: 'hello, world',
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+
+      getSlateBlockValue(cy.get('.slate-editor').last()).should('deep.eq', [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: '',
+            },
+          ],
+        },
+      ]);
+    });
   });
 }
