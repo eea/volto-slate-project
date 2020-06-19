c49b73d5c73f303cefc22135e87424f81eb5927c
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-06-19 18:40:50 +0300

    Update login.js

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -6,7 +6,7 @@ if (Cypress.env('API') !== 'guillotina') {
     });
     it('As registered user I can login', function () {
       cy.get('#login').type('admin').should('have.value', 'admin');
-      cy.get('#password').type('secret').should('have.value', 'secret');
+      cy.get('#password').type('admin').should('have.value', 'admin');
       cy.get('#login-form-submit').click();
       cy.get('body').should('have.class', 'has-toolbar');
     });
