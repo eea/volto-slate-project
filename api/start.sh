01c73f78eecafb112b297bfd71d683dc2b356a12
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-06-30 19:51:41 +0300

    Update start.sh

--- api/start.sh
+++ api/start.sh
@@ -10,5 +10,8 @@ sed -i "s#allow_origin=.*#allow_origin=\"$CORS_ALLOW_ORIGIN\"#g"  base.cfg
 bin/pip install -r requirements.txt
 echo "Start buildout"
 bin/buildout
+
+sed -i "s#allow_origin=.*#allow_origin=\"$CORS_ALLOW_ORIGIN\"#g"  eggs/kitconcept.volto-1.3.2-py3.7.egg/kitconcept/volto/cors/configure.zcml
+
 bin/robot-server plone.app.robotframework.testing.PLONE_ROBOT_TESTING
 
