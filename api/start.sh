b077b333a781ee5abd5cb057a43364a6ce0d0139
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-06-30 16:11:17 +0300

    Update start.sh

--- api/start.sh
+++ api/start.sh
@@ -6,9 +6,9 @@ python3 -m venv .
 bin/pip install --upgrade pip
 apt-get update -q 
 apt-get install -y gcc
+sed -i "s#allow_origin=.*#allow_origin=\"$CORS_ALLOW_ORIGIN\"#g"  base.cfg
 bin/pip install -r requirements.txt
 echo "Start buildout"
 bin/buildout
-sed -i "s#allow_origin=.*#allow_origin=\"$CORS_ALLOW_ORIGIN\"#"     parts/instance/etc/package-includes/999-additional-overrides.zcml
 bin/robot-server plone.app.robotframework.testing.PLONE_ROBOT_TESTING
 
